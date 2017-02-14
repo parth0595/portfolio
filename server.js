822a12d6c9fe85c3aa3bc544f5d688b00af533a6
parth0595 <parth0595@gmail.com>
2017-02-14 23:25:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title:'ARTICLE ONE | PARTH GUPTA',
+    heading:'ARTICLE ONE',
+    date:'FEB 14, 2107',
+    content: `
+     <p>
+                 THIS IS MY CONETNTTHIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.
+             </p>
+             
+              <p>
+                 THIS IS MY CONETNTTHIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.
+             </p>
+             
+              <p>
+                 THIS IS MY CONETNTTHIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.THIS IS MY CONETNT.
+             </p>
+    `
+    
+};
+function createTemplate(data){
+  var title= data.title;
+  var heading= data.heading;
+  var date= data.date;
+  var content= data.content;
+  
+var htmlTemplate= `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=decice-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+     </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">home </a>
+                 </div>
+                 <hr/>
+            <div>
+                
+              <h3>
+                ${heading}
+             </h3> 
+             </div>
+             <div>
+                ${date}
+                 
+             </div>
+             
+             ${content}
+         </div>
+     </body>    
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui','article-one.html'));
+    res.send(craeteTemplate(articleOne));
 });
 
 app.get('/article-two',function (req,res) {
