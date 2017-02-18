8b541c18e8a209fa9c04da8556165a38bf855f88
parth0595 <parth0595@gmail.com>
2017-02-18 22:05:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,10 @@ alert('hi, i love you.');
 var image=document.getElementById('madi');
  var marginLeft=0;
 function move(){
-    var marginLeft= marginLeft+10;
-    image.style.marginLeft= marginLeft + 'px';
+    //var marginLeft= marginLeft+10;
+    //image.style.marginLeft= marginLeft + 'px';
 }
 image.onclick = function() {
-var inerval = setInterval(move , 100);
+//var inerval = setInterval(move , 100);
+ image.style.marginLeft= '100px';
 };
