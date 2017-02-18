77d2d23da7bb974a4cce284f326835d9f9c3b847
parth0595 <parth0595@gmail.com>
2017-02-18 23:20:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,10 @@ function move(){
     var marginLeft = 0;
     marginLeft= marginLeft + 10;
    image.style.marginLeft= marginLeft + 'px';
-   if(marginLeft==300){
-       
-        image.style.marginLeft= '10px';
-    
-   }
+   if(marginLeft==300)
+   {
+       image.style.marginLeft= '10px';
+    }
     }
 image.onclick = function() {
 var inerval = setInterval(move , 100);
