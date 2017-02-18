10e0db5905a0802b7c618735b806141c259053e1
parth0595 <parth0595@gmail.com>
2017-02-18 23:05:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ function move(){
    image.style.marginLeft= marginLeft + 'px';
    if(marginLeft==300){
        marginLeft=marginLeft -10;
-        image.style.marginLeft= marginLeft + 'px';
+        image.style.marginLeft= '50px';
    }
     }
 image.onclick = function() {
