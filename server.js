eb58dca7825087f6b84b2e84c8d7ab5f85a41c76
parth0595 <parth0595@gmail.com>
2017-02-18 19:34:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,13 +84,7 @@ app.get('/:articleName',function (req,res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function (req,res) {
-     res.send(createTemplate(articleTwo));
-});
 
-app.get('/article-three',function (req,res) {
-   res.send(createTemplate(articleThree));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
