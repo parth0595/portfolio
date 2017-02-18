67b6fd121a17249c75d6bf3f73d84cc47ff1999b
parth0595 <parth0595@gmail.com>
2017-02-18 22:07:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ alert('hi, i love you.');
 
  // move the image
 var image=document.getElementById('madi');
-function move(){
+//function move(){
     //var marginLeft= marginLeft+10;
     //image.style.marginLeft= marginLeft + 'px';
-}
+//}
 image.onclick = function() {
 //var inerval = setInterval(move , 100);
  image.style.marginLeft= '100px';
